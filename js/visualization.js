3df177c57f59528b3144349eb88b596c6324628d
Emily Szabo <emilyszabo@Emilys-MacBook-Pro.local>
2024-11-21 12:12:01 -0500

    “comment”

--- js/visualization.js
+++ js/visualization.js
@@ -2,6 +2,19 @@
 // variables and prevent 
 ((() => {
 
-  console.log("Hello, world!");
+  d3.json("data/InfrastructureData.json", (data) => {
+  const dispatchString = "selectionUpdated";
 
-})());
+
+    let spBusPassengers = scatterplot()
+    .x(d => d.infrastructureInvestment)
+    .xLabel("Transportation Investment (USD)")
+    .y(d => d.railUsageTotalPassengers2022 / d.population)
+    .yLabel("Rail Passengers per Capita")
+    .yLabelOffset(150)
+    .selectionDispatcher(d3.dispatch(dispatchString))
+    ("#scatterplot", data);
+
+  });
+    
+})()); 
