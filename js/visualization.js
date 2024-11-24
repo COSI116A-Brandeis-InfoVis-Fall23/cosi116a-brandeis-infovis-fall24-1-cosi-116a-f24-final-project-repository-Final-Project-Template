e6c52dd40d8b3720fdc1c561553b25c84b98ee97
Madison Sansone <madisonsansone@madisons-mbp.dyn.brandeis.edu>
2024-11-23 19:16:33 -0500

    linechart axes

--- js/visualization.js
+++ js/visualization.js
@@ -4,4 +4,22 @@
 
   console.log("Hello, world!");
 
+  //temporary line chart data "junk data"
+  const linechartData = [
+    [2000, 100],
+    [2005, 200],
+    [2010, 150],
+    [2015, 300],
+    [2020, 250],
+  ];
+
+
+// Create and render the line chart
+let lcHeartDisease = linechart()
+  .xLabel("Year")
+  .yLabel("Age-Adjusted Heart Disease Death Rate")
+  .yLabelOffset(-40);
+
+lcHeartDisease("#linechart", linechartData);
+
 })());
