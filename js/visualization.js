4231777eb8518eb38288c4deeff0a84fa89c23c2
Madison Sansone <madisonsansone@Madisons-MacBook-Pro.local>
2024-12-01 14:22:53 -0500

    linechart working for one state with real data

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,6 @@
 // Immediately Invoked Function Expression to limit access to our 
 // variables and prevent 
 ((() => {
-
   console.log("Hello, world!");
 
   //temporary line chart data "junk data"
@@ -15,13 +14,31 @@
     [2022, 250],
   ];
 
+  d3.json("data/heartratesALL.json", (rates) => {
+  console.log(rates);
+
+  // Extract the years 
+  let years = Object.keys(rates); // ['1999', '2000', ...]
+  let data = years.map(year => {
+    return { 
+        year: year, 
+        value: rates[year]["Alabama"] // Heart disease death rate for Alabama
+    };
+  });
+console.log(data);
 
 // Create and render the line chart
 let lcHeartDisease = linechart()
+  .x(d => d.year)
   .xLabel("Year")
+  .y(d => d.value)
   .yLabel("Age-Adjusted Heart Disease Death Rate")
   .yLabelOffset(40)
 
-lcHeartDisease("#linechart", linechartData);
+lcHeartDisease("#linechart", data);
+}).catch((error) => {
+  console.error("Error loading the JSON file:", error);
+
+});
 
 })());
