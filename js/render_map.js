cd138f44c247d9356513f63678bfb7634396240d
AdamRieden <150611958+AdamRieden@users.noreply.github.com>
2024-12-15 03:53:14 +0000

    pop up can now zoom in/out to get a better view of the graph

--- js/render_map.js
+++ js/render_map.js
@@ -43,22 +43,56 @@ d3.json("data/states.json", function(error, topologies) {  // (4)
   .on("mouseover", function (event, d) {
     var name = state.features[d].properties.STATENAM.replace(" Territory", ""); // OMG SO IMPORTANT
 
-    // Calculate the centroid of the state
-    var centroid = path.centroid(d);
-
     d3.select("#popup")
     .style("display", "block")
-    .style("left", centroid[0] + "px") // Horizontal position
-    .style("top", centroid[1] - 60 + "px") // Vertical position, adjust for better alignment
+
     .html(`
       <strong>${name}</strong><br>
-      <img src="images/${name}.png" alt="${name}" style="max-width: 100px; display: block; margin: 10px 0;">
+      <img src="images/${name}.png" alt="${name}" style="max-width: 150px; display: block; margin: 10px 0;">
+      <p>Basic information about ${name}...</p>
       `);
 })
 .on("mouseout", function () {
   // Hide the popup when the mouse leaves
   d3.select("#popup").style("display", "none");
+  
 })
+.on("click", function (event, d) {
+  var name = state.features[d].properties.STATENAM.replace(" Territory", ""); // OMG SO IMPORTANT
+
+  // Select the popup element
+  var popup = d3.select("#popup");
+
+  // Check if the popup is already enlarged
+  var isLarge = popup.classed("large");
+
+  // Toggle the size
+  if (isLarge) {
+    // Shrink the popup
+    popup
+      .classed("large", false)
+      .style("transform", "scale(1)") // Reset size
+      .style("z-index", 1000) // Reset z-index
+      .html(`
+        <strong>${name}</strong><br>
+        <img src="images/${name}.png" alt="${name}" style="max-width: 100px; display: block; margin: 10px 0;">
+        <p>Basic information about ${name}...</p>
+      `);
+  } else {
+    // Enlarge the popup
+    popup
+      .classed("large", true)
+      .style("transform", "scale(1.5)") // Enlarge size
+      .style("z-index", 2000) // Bring to front
+      .html(`
+        <strong>${name}</strong><br>
+        <img src="images/${name}.png" alt="${name}" style="max-width: 200px; display: block; margin: 10px 0;">
+        <p>Additional detailed information about ${name}...</p>
+      `);
+  }
+
+})
+
 .append("svg:title")
 .text(function (d) {
   return d.properties.STATENAM;
